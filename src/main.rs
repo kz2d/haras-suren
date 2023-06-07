92fe27f90198b913db5627b02523768a5fb19527
kz2d <avidex111@gmail.com>
2023-06-07 15:55:20 +0000

    asd

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@ mod svg_to_jpeg;
 
 use std::{collections::HashSet, path::Path, sync::Arc};
 
-use bookbinder_common::convert_svg_to_jpg;
 use teloxide::{prelude::*, types::InputFile};
 use tokio::{fs, sync::Mutex};
+use dotenv::dotenv;
+
+use crate::svg_to_jpeg::render_profile;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
     // let mut groups = Arc::new(Mutex::new(HashSet::new()));
     pretty_env_logger::init();
     log::info!("Starting throw dice bot...");
@@ -16,16 +19,16 @@ async fn main() {
 
     teloxide::repl(bot, |bot: Bot, msg: Message| async move {
         // fs::write("pattetesth.jpg", convert_svg_to_jpg((fs::read_to_string("test.svg").await?).as_str(), Some(100)).unwrap()).await?;
-        fs::write(
-            "asdasd.jpg",
-            convert_svg_to_jpg((fs::read_to_string("test.svg").await?).as_str(), None).unwrap(),
-        )
-        .await?;
+        // fs::write(
+        //     "asdasd.jpg",
+        //     convert_svg_to_jpg((fs::read_to_string("test.svg").await?).as_str(), None).unwrap(),
+        // )
+        // .await?;
         // convert_svg_to_jpg((fs::read_to_string("test.svg").await?).as_str(), None).unwrap();
         bot.send_photo(
             msg.chat.id,
             InputFile::memory(
-                convert_svg_to_jpg((fs::read_to_string("test.svg").await?).as_str(), None).unwrap(),
+                render_profile().unwrap(),
             ),
         )
         .await?; // InputFile::url(url::Url::parse("https://leetcode-stats-six.vercel.app/?username=KnlnKS").unwrap())).await?;
--- src/svg_to_jpeg.rs
+++ src/svg_to_jpeg.rs
@@ -1,70 +1,8 @@
 use core::fmt;
-use std::error::Error;
+use std::{error::Error, process::Command, fs::{self, read}, io};
 
-use image::{codecs::jpeg::JpegEncoder, ColorType};
-use usvg::{Tree, Image};
-use resvg::{render, tiny_skia::{Transform, PixmapMut}};
-
-fn render_svg(svg: &str, dpi: Option<usize>, alpha: bool) -> Result<Image, SvgError> {
-    let mut options = usvg::Options::default();
-    if let Some(dpi) = dpi {
-        options.dpi = dpi as f64;
-    }
-    let tree = Tree::from_str(svg, &options)?;
-    let fit_to = usvg::FitTo::Original;
-	let mut pixmap = PixmapMut::;
-    let background = if alpha {
-        None
-    } else {None
-        // Some(svgtypes::Color::white())
-    };
-    render(&tree, fit_to, Transform::default(), pixmap)?;
-	return pixmap
-}
-
-/// Convert a svg str to a jpg file
-pub fn convert_svg_to_jpg(svg: &str, dpi: Option<usize>) -> Result<Vec<u8>, SvgError> {
-    let image = render_svg(svg, dpi, false)?;
-    let width = image.width();
-    let height = image.height();
-
-    let mut jpeg = Vec::new();
-    let mut encoder = JpegEncoder::new(&mut jpeg);
-    encoder.encode(image.data(), width, height, ColorType::Rgb8)?;
-    Ok(jpeg)
-}
-
-
-#[derive(Debug)]
-pub enum SvgError {
-    Unspecified,
-    Usvg(usvg::Error),
-    Image(image::error::ImageError),
-    Io(std::io::Error),
-}
-
-impl Error for SvgError {}
-
-impl fmt::Display for SvgError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:?}", self)
-    }
-}
-
-impl From<usvg::Error> for SvgError {
-    fn from(src: usvg::Error) -> Self {
-        SvgError::Usvg(src)
-    }
-}
-
-impl From<image::error::ImageError> for SvgError {
-    fn from(src: image::error::ImageError) -> Self {
-        SvgError::Image(src)
-    }
-}
-
-impl From<std::io::Error> for SvgError {
-    fn from(src: std::io::Error) -> Self {
-        SvgError::Io(src)
-    }
+pub fn render_profile() -> io::Result<Vec<u8>> {
+    Command::new("convert-svg-to-png").arg("test.svg").spawn().unwrap().wait().unwrap();
+    
+    read("test.png")
 }
